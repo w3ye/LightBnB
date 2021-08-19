080c8bbb01fcbd111c8e1f426edf39453f658224
William Ye <w3ye@pm.me>
2021-08-19 14:19:04 -0400

    implemented getUserWithEmail

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -17,16 +17,14 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const query = `SELECT * FROM users WHERE email = $1;`;
+  return pool.query(query, [email])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch(err => {
+      return null;
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
